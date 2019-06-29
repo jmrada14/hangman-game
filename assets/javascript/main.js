d7409ff4066099f2ec5768505eeab2914231588a
jmrada14 <jmrada14@tutanota.com>
2019-06-29 00:18:03 -0400

    last bug fixed, script working fine

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,33 +1,28 @@
 window.onload = function() {
   
-    // document get by ID variables
+    // getbyid vars
     var hiddenWord = document.getElementById("hiddenWord");
     var lives1 = document.getElementById("lives");
     var guesses1 = document.getElementById("guessedWords");
     var newGame1 = document.getElementById("newGame");
-    //array of words
+    //vars
     var words = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn',]
     var alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
   
     function newGame() {
-      // reset score
-      var lives = 9;
+      var lives = 6;
       lives1.innerHTML = lives;
-      
-      // GATHER USER GUESS AND DISPLAY
       var storedGuess = "";
       var arrayGuess = [];
       guesses1.innerHTML = arrayGuess;
-      
-      // game progress variables keep track of solved letters
       var dash = 0;
       var letters = 0;
       
-      // pick random item from array
+      // random word
       var randomWord = words[Math.floor(Math.random()*words.length)];
       console.log(randomWord);
   
-      //display random word in document as blank spaces
+      //displays random word
       var splitWord = randomWord.split('');
       console.log(splitWord);
       var arrayOfBlanks = splitWord.map(function(val) {
@@ -38,29 +33,24 @@ window.onload = function() {
           return "-";
         }
       });
-      // join the blanks back together into a string to display on the page
+      // join blanks
       var stringOfBlanks = arrayOfBlanks.join('');
-      // place word blanks on page
+      // word blanks on place
       hiddenWord.innerHTML = stringOfBlanks;
-    
-      // keyup function
+
       document.onkeyup = function checkKey() {
         var keyPress = event.keyCode
         storedGuess = String.fromCharCode(keyPress).toLowerCase();
-  
-        // IF STATEMENT TO CHECK IF GUESS IS A LETTER
         if (alphabet.indexOf(storedGuess) !== -1 && arrayGuess.indexOf(storedGuess) === -1)  {
           arrayGuess.push(storedGuess);  
           guesses1.innerHTML = arrayGuess;
           console.log(arrayGuess);
-          // IF STATEMENT TO SEE IF GUESS MATCHES WORD
           for (var i = 0; i < splitWord.length; i++) {
             if (storedGuess == splitWord[i].toLowerCase()) {
               arrayOfBlanks[i] = storedGuess;
               hiddenWord.innerHTML = arrayOfBlanks.join('').toUpperCase();
               letters++;
               console.log(letters);
-              // TO WIN
               if (letters + dash === randomWord.length) {
                 lives1.innerHTML = "YOU WON";
                 newGame1.innerHTML = 'PRESS "ENTER" TO CONTINUE';
@@ -71,12 +61,12 @@ window.onload = function() {
                   newGame();
                   newGame1.style.visibility = "hidden";
                   }
-                } // Reset the game
-              } // END WIN CONDITION
-            } // END IF MATCH 
-          } // END MATCH LOOP
+                } 
+              } 
+            } 
+          }
           
-        // DECREASING LIVES IF WRONG GUESS
+        
         if (randomWord.indexOf(storedGuess) === -1) {
           lives -= 1;
           lives1.innerHTML = lives;
@@ -90,14 +80,14 @@ window.onload = function() {
               newGame();
               newGame1.style.visibility = "hidden";
               }
-            } // Reset the game
-          } // End of lose condition
-        } // END DECREASE LIVES
-      } // END OF ALPHABET CHECK 
-    } // end of onkeyup function
-   } // end newGame();
+            } 
+          } 
+        } 
+      }  
+    } 
+   }
    newGame();
-  } //end of page ready
+  } 
   
    
  
